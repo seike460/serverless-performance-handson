39a5d89f05f3636549e97a62c19107727a414924
seike460 <seike460@gmail.com>
2024-03-02 08:18:24 +0900

    ちょっとだけ負荷上げる

--- generate-payload.js
+++ generate-payload.js
@@ -3,7 +3,7 @@
 module.exports.generatePayload = (context, events, done) => {
   context.vars.payload = {
     customerId: `customer${Math.floor(Math.random() * 1000)}`,
-    orderId: `order${Math.floor(Math.random() * 100000)}`,
+    orderId: `order${Math.floor(Math.random() * 1000)}`,
     items: [{ productId: "product789", quantity: 1 }],
     totalAmount: Math.floor(Math.random() * 1000)
   };
